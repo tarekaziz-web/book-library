e13c50860046ce37ccb9d67e408ec3a1e205ff33
tarekaziz-web <tarekaziz.web@gmail.com>
2025-10-05 20:19:05 +0600

    book

---
+++ src/lib/utils.ts
@@ -0,0 +1,30 @@
+
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function capitalize(str: string) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+type GroupBy<T, K extends keyof T> = Record<string, T[]>;
+
+export function groupBy<T, K extends keyof T>(
+  array: T[],
+  key: K
+): GroupBy<T, K> {
+  return array.reduce((acc, item) => {
+    const keyValue = String(item[key]);
+    if (!acc[keyValue]) {
+      acc[keyValue] = [];
+    }
+    acc[keyValue].push(item);
+    return acc;
+  }, {} as GroupBy<T, K>);
+}
+
+
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,16 @@
+
+import Banner from "@/pages/home/Banner";
+
+import MainSection from "@/pages/home/MainSection";
+
+
+function HomePage() {
+    return (
+        <>
+            <Banner />
+            <MainSection />
+        </>
+    );
+}
+
+export default HomePage;
